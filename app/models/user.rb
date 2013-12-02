9853bfa60e50846b1a7ae356eebed3c61ac4f3d0
Quentin Logie <quentin.logie@epsi.fr>
2013-12-02 09:17:40 +0100

    Update model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
+  validates :birthdate,
+            presence: true
+
   validates :firstname,
             presence: true
 
   validates :lastname,
-            presence: true
-
-  validates :birthdate,
             presence: true
 end
--- db/migrate/20131202075449_create_users.rb
+++ db/migrate/20131202075449_create_users.rb
@@ -1,14 +1,14 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string :avatar
+      t.string :birthdate
+      t.string :country_code
       t.string :firstname
       t.string :lastname
-      t.string :avatar
       t.string :phone
-      t.string :birthdate
-      t.string :town
       t.string :street
-      t.string :country_code
+      t.string :town
 
       t.timestamps
     end
