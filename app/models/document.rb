d63fb59a78fffff9bd69cc641e86aa93aca9b84a
benjaminsenechal <benjamin.senechal1@epsi.fr>
2014-01-07 12:01:41 +0100

    Add statemachine for Documents

--- app/models/document.rb
+++ app/models/document.rb
@@ -4,6 +4,25 @@ class Document < ActiveRecord::Base
 
   has_paper_trail
 
+  state_machine :state, initial: :wip do
+    event :ask_for_review do
+      transition wip: :in_review
+    end
+
+    event :rejected do
+      transition in_review: :wip
+      transition validate: :wip
+    end
+
+    event :validated do
+      transition in_review: :validate
+    end
+
+    event :archived do
+      transition validate: :archive
+    end
+  end
+
   validates :description,
             presence: true
 
