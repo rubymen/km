fea63f32d87e228c038475be573d600bf25164af
Nicolas Devynck <nicolas.devynck@epsi.fr>
2014-01-07 12:17:16 +0100

    Add elasticsearch functionality

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,8 +1,25 @@
 class Document < ActiveRecord::Base
   extend FriendlyId
+  include Tire::Model::Search
+  include Tire::Model::Callbacks
+
   friendly_id :title, use: :slugged
 
   has_paper_trail
+
+  mapping do
+    indexes :content,     type: :string
+    indexes :description, type: :string
+    indexes :title,       type: :string
+  end
+
+  def to_indexed_json
+    {
+      content: self.content,
+      description: self.description,
+      title: self.title,
+    }.to_json
+  end
 
   validates :description,
             presence: true
