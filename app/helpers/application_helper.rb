bbf5074bc430dd0062db6d11515f702e2b7f6fc0
Rémi Delhaye <remi.delhaye@epsi.fr>
2014-02-22 15:39:15 +0100

    Add meta tags for better SEO

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def keywords
+    'documents, milicon, knowledge, management, manager, km, knowledge management, knowledge-management, knowledgemanagement, rubymen, ruby on rails, document, tag'
+  end
+
   def to_b field
     field.to_i == 0 ? false : true
   end
