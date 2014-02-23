f76684047295881af87b7c24de14d4d11473eac7
NOURTIER Antoine <a.nourtier59@gmail.com>
2014-02-23 11:45:33 +0100

    Fix done

--- db/migrate/20140113095537_create_comments.rb
+++ db/migrate/20140113095537_create_comments.rb
@@ -1,6 +1,7 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
+      t.string :title
       t.string :content
       t.references :user, index: true
       t.references :comment, index: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140220173441) do
+ActiveRecord::Schema.define(version: 20140223100328) do
 
   create_table "attachments", force: true do |t|
     t.string   "path"
