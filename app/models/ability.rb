e175bbc516dd511210dc4cc105b5f986968cf3a3
benjaminsenechal <benjamin.senechal1@epsi.fr>
2014-03-17 15:48:35 +0100

    Add private comments

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,17 +8,26 @@ class Ability
 
     if user && user.class.name === 'Users::Contributor'
       can :create, Document
-      can :read, Document do |document|
-        document.state.include?('archive') || document.state.include?('validate') || user.document_ids.include?(document.id)
-      end
       can :state, Document, id: user.document_ids, state: ['wip']
       can [:update, :comment], Document, id: user.document_ids, state: ['in_review', 'wip']
       can [:read, :update, :destroy], User, id: user.id
+
+      can :read, Comment do |comment|
+        (comment.readable? || comment.document.user_ids.include?(user.id)) || comment.readable?
+      end
+
+      can :read, Document do |document|
+        document.state.include?('archive') || document.state.include?('validate') || user.document_ids.include?(document.id)
+      end
     end
 
     if user && user.class.name === 'Users::Member'
       can :read, Document, state: ['archive', 'validate']
       can [:read, :update, :destroy], User, id: user.id
+
+      can :read, Comment do |comment|
+        (comment.readable? || comment.document.user_ids.include?(user.id)) || comment.readable?
+      end
     end
   end
 end
